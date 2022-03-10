7a14792abef5fa6bc1daa25a322f6bd74928161d
hp <hp@LAPTOP-1R18O0NV>
2022-03-10 16:16:42 +0530

    Day8

---
+++ DAY8/src/Inheritance.java
@@ -0,0 +1,21 @@
+
+class Parent
+{
+	void disp()
+	{
+		System.out.println("parent class");
+	}
+}
+
+
+public class Inheritance extends Parent
+{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Inheritance i=new Inheritance();
+        		i.disp();
+        
+	}
+
+}
---
+++ DAY8/src/Seltos.java
@@ -0,0 +1,30 @@
+
+abstract class Mumbai_Rules
+{
+	abstract void speedlimit();
+	abstract void rashdriving();
+	
+	{
+		System.out.println("no fine");
+	}
+}
+public class Seltos extends Mumbai_Rules
+{
+     void speedlimit()
+     {
+    	System.out.println("The limit is 80") ;
+     }
+     void rashdriving()
+     {
+    	 System.out.println("Alert voice");
+     }
+	public static void main(String[] args){
+	
+		 Seltos s=new Seltos();
+		 s.speedlimit();
+		 s.rashdriving();
+		 
+
+	}
+
+}
