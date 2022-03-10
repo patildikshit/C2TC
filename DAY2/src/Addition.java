c91e1b3ece903a8b33bab47e291a2764d86f4b71
hp <hp@LAPTOP-1R18O0NV>
2022-03-10 17:13:03 +0530

    DAY2

---
+++ DAY2/src/Addition.java
@@ -0,0 +1,15 @@
+
+public class Addition {
+
+	public static void main(String[] args)
+	{
+	
+		int a = 951;
+		int b = 49;
+		int c = a+b;
+		
+		System.out.println("The sum of a & b " +c);
+
+	}
+
+}
