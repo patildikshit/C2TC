400b080e70b8947e384cfd33d2cafe835aff6201
hp <hp@LAPTOP-1R18O0NV>
2022-03-12 11:54:33 +0530

    Day10

---
+++ DAY10/src/DecisionMaking.java
@@ -0,0 +1,26 @@
+
+public class DecisionMaking {
+
+	public static void main(String[] args) 
+	{
+		
+		int num=178;
+		
+		if(num<100)
+		{
+			System.out.println("Given number is less than 100 ");
+		}
+		
+		if(num>50)
+		{
+			System.out.println("Given number is less than 100 ");
+		}
+			
+		else
+		{
+			System.out.println("Given number is not less than 100");
+		}
+
+	}
+
+}
---
+++ DAY10/src/DemoVar.java
@@ -0,0 +1,30 @@
+
+public class DemoVar {
+
+	public static void main(String[] args) 
+	{
+		byte num=68;   //-128 to 127
+		System.out.println(num);
+
+		short num1=4679;   //-32768 to 32767
+		System.out.println(num1);
+		
+		float num2=34.23f;
+		System.out.println(num2);
+		
+		double num3=758.369;
+		System.out.println(num3);
+		
+		char ch='d';
+		System.out.println(ch);
+		
+		String s="Happy weekend";
+		System.out.println(s);
+		
+		final int a=89;
+		System.out.println(a);
+		
+		
+	}
+
+}
